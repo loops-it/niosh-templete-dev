5a2e868b6be4b64d4eb8f1b9c3df2bdcfea3a938
tj <thinupavani@gmail.com>
2024-11-27 19:29:03 +0530

    testimonial sllider fixed

--- script/script.js
+++ script/script.js
@@ -1,39 +1,31 @@
+// let slideIndex = 0;
+// showSlides();
 
+// function showSlides() {
+//   let i;
+//   const slides = document.getElementsByClassName("testimonial-slide");
+//   const dots = document.getElementsByClassName("dot");
 
-let slideIndex = 0;
-showSlides();
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
 
-function showSlides() {
-  let i;
-  const slides = document.getElementsByClassName("testimonial-slide");
-  const dots = document.getElementsByClassName("dot");
- 
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  
+//   slideIndex++;
+//   if (slideIndex > slides.length) {
+//     slideIndex = 1;
+//   }
 
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  
- 
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  
-  
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-  
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
 
-  setTimeout(showSlides, 4500);
-}
+//   slides[slideIndex - 1].style.display = "block";
+//   dots[slideIndex - 1].className += " active";
 
-function currentSlide(n) {
-  slideIndex = n - 1; 
-  showSlides();
-}
+//   setTimeout(showSlides, 4500);
+// }
 
-
+// function currentSlide(n) {
+//   slideIndex = n - 1;
+//   showSlides();
+// }
